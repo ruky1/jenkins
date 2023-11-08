56a3fa50ae3a5b809383aeca3cd6eb1211c1e52e
ruky1 <pou11uyt@gmail.com>
2023-11-08 12:48:51 +0900

    Jenkins start

---
+++ src/main/java/boot/data/ShopController.java
@@ -0,0 +1,17 @@
+package boot.data;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ShopController {
+	
+	@GetMapping("/shop/list")
+	public ModelAndView list() {
+		ModelAndView model=new ModelAndView();
+		
+		model.setViewName("shop/shoplist");
+		return model;
+	}
+}
